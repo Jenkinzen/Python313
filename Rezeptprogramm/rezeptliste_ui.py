9091d05ba90a72dd4166a3a6897e5f09017447cc
Gordon <ggordonhuesch94@gmail.com>
2026-02-03 15:41:47 +0100

    UI zurückfunktion in der Menüsteuerung / komplette Menüführung auf Zahleninput gestellt

--- Rezeptprogramm/rezeptliste_ui.py
+++ Rezeptprogramm/rezeptliste_ui.py
@@ -3,13 +3,23 @@ import rezeptliste_model as model
 neustart = True
 
 def filter_auswaehlen():
-    return input("Nach welchen Kriterien soll die Rezeptauswahl gefiltert werden?\n"
-                 "[Gerichte]\n[Zutaten]\noder\n[Gang]?\n").strip().lower()
+    return input("Nach welchen Kriterien soll die\n"
+                "Rezeptauswahl gefiltert werden?\n\n"
+                 "1-[Gerichte]\n"
+                 "2-[Zutaten]\n"
+                 "3-[Gang]\n\n"
+                 "geben sie [0] ein um zurück zum Menü zu gelangen:\n").strip().lower()
+
 
 def rezepte_ansehen_nach_gang():
     while True:
-        gangeingabe = input("Welchen Gang möchten Sie wählen?\n").strip().lower()
+        gangeingabe = input("Welchen Gang möchten Sie wählen,\n"
+        "[Vorspeise],[Hauptspeise] oder[Dessert]?\n" \
+        "Geben sie [ 0 ] ein um zurück zum Menü zu gelangen:\n").strip().lower()
         rezepte = service.filter_rezepte_nach_gang(gangeingabe)
+
+        if gangeingabe == "0":
+            return
 
         if not rezepte:
             print("Keine Rezepte gefunden.")
@@ -38,10 +48,17 @@ def rezepte_ansehen_nach_gang():
 
 def rezepte_ansehen_nach_zutaten():
     while True:
-        zutat = [z.strip() for z in input("Welche Zutat(en) möchten Sie wählen?\nBitte Zutaten mit , trennen.\n").strip().lower().split(",")]
+        zutat = [z.strip() for z in input("Welche Zutat(en) möchten Sie wählen?\nBitte Zutaten mit , trennen.\n" \
+        "geben sie [0] ein um zurück zur Filterauswahl zu gelangen:\n").strip().lower().split(",")]
+
+        if zutat == ["0"]:
+            return
+        
         rezepte = service.filter_rezepte_nach_zutaten(zutat)
 
-        if not rezepte:
+        
+
+        if not rezepte :
             print("Keine Rezepte gefunden.")
             continue    
 
@@ -68,20 +85,23 @@ def rezepte_ansehen_nach_zutaten():
                 print("Bitte eine Zahl eingeben.")  
                 #hier brauch man kein continue weil die Schleife hier eh endet und dann wieder anfängt
             
-                
-
 def rezepte_ansehen_nach_Gericht():
-    for i, rezept in enumerate(service.alle_rezeptnamen(), start=1):
-        print(f"{i}. {rezept}")
     while True:
+        for i, rezept in enumerate(service.alle_rezeptnamen(), start=1):
+            print(f"{i}. {rezept}")
         gerichte = service.filter_rezepte_nach_gericht("")
-        nummer = int(input("Wählen sie bitte die Nummer des Gerichtes:\n"))
+        nummer = int(input("Wählen sie bitte die Nummer des Gerichtes,\n"
+        "geben sie 0 ein um zurück zur Kriterienauswahl zu gelangen:\n"))
+
+        if nummer == 0:
+            return
+
         if nummer > len(gerichte):
             print("Falsche Zahl eingegeben!")
             continue
         rezepte = service.rezept_nach_index(gerichte,nummer)
 
-        if not rezepte:
+        if not rezepte or 0:
             print("Kein Rezept passt zu diesen Angaben.")
             continue
 
@@ -94,23 +114,32 @@ def rezepte_ansehen():
     while True:
         filterwahl = filter_auswaehlen()
 
-        if filterwahl == "zutaten":
-            rezepte_ansehen_nach_zutaten()
+        if filterwahl == "0":
             return
         
-        elif filterwahl == "gang":
-            rezepte_ansehen_nach_gang()
-            return
-        
-        elif filterwahl == "gerichte":
+        elif filterwahl == "1":
             rezepte_ansehen_nach_Gericht()
-            return
+            continue
+
+        elif filterwahl == "2":
+            rezepte_ansehen_nach_zutaten()
+            continue
+        
+        elif filterwahl == "3":
+            rezepte_ansehen_nach_gang()
+            continue
+        
         
         else:
             print("Ungültige Auswahl.")
 
 def rezept_einfuegen():
-    rezeptname = input("Wie heißt das Rezept?").strip()
+    rezeptname = input("Wie heißt das Rezept?" \
+    "geben sie 0 ein um zurück zum Menü zu gelangen:\n").strip()
+
+    if rezeptname == "0":
+        return
+
     zutaten_input = input("Welche Zutaten in welcher Menge brauch es?( Zutaten bitte mit , trennen[z.B. Enokis 200 g, Salz 2 Prise])")
     zutaten_strings = [z.strip() for z in zutaten_input.split(",")if z.strip()]
     zubereitung = input("Wie wird es zubereitet?") 
@@ -135,7 +164,11 @@ def rezept_loeschen():
         while True:
             try:
                 gerichte = service.alle_rezepte()
-                nummer = int(input("Welche Nummer soll gelöscht werden?"))
+                nummer = int(input("Welche Nummer soll gelöscht werden?"
+                "geben sie 0 ein um zurück zum Menü zu gelangen:\n"))
+
+                if nummer == "0":
+                    break
 
                 if nummer > len(service.alle_rezepte()):  
                 # eigentlich wäre es klüger len(gerichte) zu nutzen weil sonst alle_rezepte 2 mal aufgerufen wird aber für jetzt
@@ -151,11 +184,14 @@ def rezept_loeschen():
 
                 for zeile in rezept_zum_loeschen.anzeigen():
                     print(zeile)
+                    
                 rueckversichern = input(f"Sind sie sicher, dass {rezept_zum_loeschen.name} gelöscht werden soll? Ja/Nein").strip().lower()
+
                 if rueckversichern.strip().lower() == "ja":
                         service.rezept_loeschen(rezept_zum_loeschen)
                         print(f"{rezept_zum_loeschen} wurde gelöscht!")
                         return
+                
                 else:
                     print(f"Das {rezept_zum_loeschen} wird nicht gelöscht.")
                     break
@@ -167,29 +203,34 @@ service.rezept_laden()
 
 while neustart:
 
-    Menueauswahl = input("""Möchten sie ein Rezept 
-[ansehen]              [einfügen]              [löschen]?
-""")
+    Menueauswahl = input("""Möchten sie ein Rezept \n"
+1-[ansehen],
+2-[einfügen],
+3-[löschen]"?\n:""")
 
-    if Menueauswahl == "einfügen":
+
+
+    if Menueauswahl == "1":
+
+        rezepte_ansehen()
+
+
+
+    elif Menueauswahl == "2":
 
         rezept_einfuegen()
 
 
 
-
-    elif Menueauswahl == "ansehen":
-
-            rezepte_ansehen()
-
-
-
-
-
-    elif Menueauswahl == "löschen":
+    elif Menueauswahl == "3":
         
         rezept_loeschen()
         
+
+
+    else:
+        print("Ungültige Auswahl!")
+        continue
         
 
 
